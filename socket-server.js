fa0ef97ca78f28f2e6d007633bb84a3383add074
aguspramono <agus.pramono3545@gmail.com>
2026-06-19 11:22:28 +0700

    upload socket

---
+++ socket-server.js
@@ -0,0 +1,58 @@
+const express = require("express")
+const http = require("http")
+const { Server } = require("socket.io")
+const cors = require("cors")
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    },
+})
+
+
+io.on("connection", (socket) => {
+    console.log("User connected:", socket.id)
+
+    socket.on("join-order", (orderId) => {
+        socket.join(`order-${orderId}`)
+    })
+})
+
+
+app.post("/send-location", (req, res) => {
+    const data = req.body
+
+    io.to(`order-${data.order_id}`).emit("receive-location", data)
+
+    res.json({ success: true })
+})
+
+
+app.post("/send-position", (req, res) => {
+    const data = req.body
+
+    io.emit("receive-position", data)
+
+    res.json({ success: true })
+})
+
+
+app.post("/update-position", (req, res) => {
+    const data = req.body
+
+    io.emit("get-position", data)
+
+    res.json({ success: true })
+})
+
+
+
+server.listen(3001, () => {
+    console.log("Socket server running on port 3001")
+})
